5d6bc0af2030110979d91a22bca2e0a53278dd25
salochan <salochan@amd.com>
2024-03-13 18:42:54 +0530

    Siena: Added kernel configuration
    -SMBus,UART,IOMMU and I2C Kernel configuration added
    -Added Inejction module kernel configuration to support AMD RAS tool
    -Added NDCTL Kernel configuration
    
    Change-Id: I652fa1ca478f0429c36ce8e44cf029816d371fe6

--- setup_siena.sh
+++ setup_siena.sh
@@ -20,8 +20,7 @@ git clone https://git.yoctoproject.org/meta-virtualization -b kirkstone
 #git clone -b master https://github.com/Wind-River/meta-secure-core.git
 
 # Clone meta-amd repository using SSH
-git clone https://git.yoctoproject.org/meta-amd.git -b kirkstone-amd-epg
-
+git clone "ssh://gerritgit/eesc/ec/ese/meta-amd"
 # Clone for tpm-tools
 #git clone -b kirkstone https://github.com/Wind-River/meta-secure-core.git
 git clone -b kirkstone https://git.yoctoproject.org/git/meta-security
@@ -32,4 +31,6 @@ cd meta-openembedded
 git checkout --quiet 402affcc073db39f782c1ebfd718edd5f11eed4c
 cd ../meta-dpdk
 git checkout --quiet 12cd9701455f17ff86681dba86e5c10f7b0deb7c
+cd ../meta-amd
+git checkout kirkstone
 
